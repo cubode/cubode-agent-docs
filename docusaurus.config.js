af86722146a2f44ba7d521b81ff82ce408321907
Castelli1987 <davide.castelli@cubode.com>
2024-09-06 13:39:26 +0100

    modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/cubodeLogo.svg',
 
   // Set the production url of your site here
-  url: 'https://Castelli1987.github.io',
+  url: 'https://cubode.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Cubode-Agent-Docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Castelli1987', // Usually your GitHub org/user name.
+  organizationName: 'cubode', // Usually your GitHub org/user name.
   projectName: 'Cubode-Agent-Docs', // Usually your repo name.
 
   trailingSlash: true, // or false, based on your URL preference
